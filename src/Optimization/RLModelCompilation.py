73491060e5757809ff6823218114351ce236fc4b
MarcusHjertaas <marcus.hjertaas@gmail.com>
2025-03-30 21:16:44 +0200

    robin

--- src/Optimization/RLModelCompilation.py
+++ src/Optimization/RLModelCompilation.py
@@ -11,6 +11,7 @@ from stable_baselines3.common.vec_env import DummyVecEnv, VecNormalize
 
 from sklearn.model_selection import train_test_split
 
+from stable_baselines3.common.evaluation import evaluate_policy
 from src.Optimization.Environment import PortfolioEnvironment as PorEnv
 from src.Optimization.NeuralNet import CustomNeuralNet as CusNN
 from src.Optimization.NeuralNet import CustomSACPolicy as CSACP
@@ -88,6 +89,28 @@ class RL_Model():
 
         self.model = model
 
+        obs, info = train_env.reset()
+        weights_history_t = []
+        finished = False
+
+        while not finished: 
+            action, _ = model.predict(obs, deterministic=True)
+
+            weights_t = (action+1) / 2
+            weights_t /= np.sum(weights_t)
+
+            obs, reward, terminated, truncated, info = train_env.step(action)
+            finished = terminated or truncated
+
+            weights_history_t.append(weights_t)
+
+        weight_df_t  = pd.DataFrame(weights_history_t)
+        weight_df_t.to_csv("Data/RL_weights_t_"+self.objective+"_esg_"+str(self.esg_compliancy)+".csv", 
+                          index=False)
+        
+        mean_reward, std_reward = evaluate_policy(model, train_env, n_eval_episodes=10)
+        print(f"mean_reward:{mean_reward:.2f} +/- {std_reward:.2f}")
+
 
 
     def test_model(self):
@@ -109,13 +132,17 @@ class RL_Model():
         while not finished: 
             action, _ = self.model.predict(obs, deterministic=True)
 
-            weights = np.exp(action+1e-8)
-            weights = weights / (np.sum(weights))
+            weights = (action+1) / 2
+            weights /= np.sum(weights)
 
             obs, reward, terminated, truncated, info = test_env.step(weights)
             finished = terminated or truncated
 
             weights_history.append(weights)
+
+            
+        mean_reward, std_reward = evaluate_policy(self.model, test_env, n_eval_episodes=10)
+        print(f"mean_reward:{mean_reward:.2f} +/- {std_reward:.2f}")
 
 
 
